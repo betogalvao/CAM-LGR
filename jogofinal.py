4f7903e3a67e4de39932b1d268ddc04a760e8bf6
Beto Galvao <carlosamg1@al.insper.edu.br>
2021-04-28 15:57:58 -0300

    pular linha entre os primeiros prints

--- jogofinal.py
+++ jogofinal.py
@@ -2,14 +2,14 @@ import funcoesEP2 as ep2
 
 print('Paciência Acordeão') 
 print('==================')
-print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha.')
+print('Seja bem-vindo(a) ao jogo de Paciência Acordeão! O objetivo deste jogo é colocar todas as cartas em uma mesma pilha. \n')
 print('Existem apenas dois movimentos possíveis: ')
 print('1. Empilhar uma carta sobre a carta imediatamente anterior; ')
-print('2. Empilhar uma carta sobre a terceira carta anterior. ')
+print('2. Empilhar uma carta sobre a terceira carta anterior. \n')
 print('Para que um movimento possa ser realizado basta que uma das duas condições abaixo seja atendida: ')
 print('1. As duas cartas possuem o mesmo valor ou ')
-print('2. As duas cartas possuem o mesmo naipe. ') 
-print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. ')
+print('2. As duas cartas possuem o mesmo naipe. \n') 
+print('Desde que alguma das condições acima seja satisfeita, qualquer carta pode ser movimentada. \n')
 
 input('Aperte [Enter] para iniciar o jogo...')
 
